5209cf72f3383d1341fefbadaf89992b54e414bb
Riadh Benchouche <r.benchouche1@gmail.com>
2023-12-09 00:28:41 +0100

    add SpeedInsights vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {type Metadata} from 'next'
 import {RootLayout} from '@/components/RootLayout'
 
 import '@/styles/tailwind.css'
+import {SpeedInsights} from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
     title: {
@@ -16,6 +17,7 @@ export default function Layout({children}: { children: React.ReactNode }) {
         <html lang="fr" className="h-full bg-neutral-950 text-base antialiased">
         <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
+        <SpeedInsights/>
         </body>
         </html>
     )
